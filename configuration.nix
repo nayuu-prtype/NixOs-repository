ff90f92f3a5770d55c3bbc052d2b8154a6bc85a5
bradford <lucasdaniel.prtype@proton.me>
2026-01-25 20:32:07 -0300

    modularization locale-settings

--- configuration.nix
+++ configuration.nix
@@ -5,6 +5,7 @@
     [
       ./hardware-configuration.nix
       ./spicetify.nix
+      ./locale-settings.nix
     ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -38,23 +39,23 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "America/Recife";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "pt_BR.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "pt_BR.UTF-8";
-    LC_IDENTIFICATION = "pt_BR.UTF-8";
-    LC_MEASUREMENT = "pt_BR.UTF-8";
-    LC_MONETARY = "pt_BR.UTF-8";
-    LC_NAME = "pt_BR.UTF-8";
-    LC_NUMERIC = "pt_BR.UTF-8";
-    LC_PAPER = "pt_BR.UTF-8";
-    LC_TELEPHONE = "pt_BR.UTF-8";
-    LC_TIME = "pt_BR.UTF-8";
-  };
+#  # Set your time zone.
+#  time.timeZone = "America/Recife";
+#
+#  # Select internationalisation properties.
+#  i18n.defaultLocale = "pt_BR.UTF-8";
+#
+#  i18n.extraLocaleSettings = {
+#    LC_ADDRESS = "pt_BR.UTF-8";
+#    LC_IDENTIFICATION = "pt_BR.UTF-8";
+#    LC_MEASUREMENT = "pt_BR.UTF-8";
+#    LC_MONETARY = "pt_BR.UTF-8";
+#    LC_NAME = "pt_BR.UTF-8";
+#    LC_NUMERIC = "pt_BR.UTF-8";
+#    LC_PAPER = "pt_BR.UTF-8";
+#    LC_TELEPHONE = "pt_BR.UTF-8";
+#    LC_TIME = "pt_BR.UTF-8";
+#  };
 
   xdg.portal = {
     enable = true;
---
+++ locale-settings.nix
@@ -0,0 +1,21 @@
+{ configs, pkgs, ... }:
+
+{
+  # Set your time zone.
+  time.timeZone = "America/Recife";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "pt_BR.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pt_BR.UTF-8";
+    LC_IDENTIFICATION = "pt_BR.UTF-8";
+    LC_MEASUREMENT = "pt_BR.UTF-8";
+    LC_MONETARY = "pt_BR.UTF-8";
+    LC_NAME = "pt_BR.UTF-8";
+    LC_NUMERIC = "pt_BR.UTF-8";
+    LC_PAPER = "pt_BR.UTF-8";
+    LC_TELEPHONE = "pt_BR.UTF-8";
+    LC_TIME = "pt_BR.UTF-8";
+  };
+}
